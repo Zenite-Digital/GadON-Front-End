01a8afa3b9f7a33a5bed146eaf181422ff7b1951
Eduardo Garcia <eduardobgarciax134abc39@gmail.com>
2025-05-10 11:18:28 -0300

    Initial Commit

---
+++ src/components/template-component/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+        width: 200,
+        height: 52,
+        backgroundColor: "#4545e0",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#fff"
+    }
+})
