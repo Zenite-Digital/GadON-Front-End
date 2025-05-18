04804aec9585e2556f2ef8b7b9cc0630d5533295
Vitor Garcia <vitorpso270@gmail.com>
2025-05-18 17:40:56 -0300

    [ FEAT ] Estilização Outline dos Botões

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,15 @@
 import Colors from "@constants/Colors";
 import { cn } from "@utils/cn";
 import React, { useMemo } from "react";
-import { Text, TouchableHighlight } from "react-native";
+import {
+  Pressable,
+  PressableProps,
+  Text,
+  TouchableHighlight,
+} from "react-native";
+import { buttonColors } from "./styles";
 
-type ButtonProps = {
+type ButtonProps = PressableProps & {
   color?:
     | "main"
     | "primary"
@@ -15,82 +21,29 @@ type ButtonProps = {
     | "light"
     | "dark";
   fullWidth?: boolean;
-  variant?: "default" | "outline" | "text";
+  variant?: "outline" | "solid";
   text?: string;
   slots?: {
     text?: {
       className?: string;
     };
   };
-  onPress?: () => void;
   className?: string;
-};
-
-const colors = {
-  main: {
-    button: "bg-brand-main active:bg-brand-secondary",
-    text: "text-white",
-    buttonHighlight: Colors.brand.secondary,
-  },
-  primary: {
-    button: "bg-estados-normal-primary active:bg-estados-hover-primary",
-    text: "text-white",
-    buttonHighlight: Colors.estados.hover.primary,
-  },
-  secondary: {
-    button: "bg-estados-normal-secondary active:bg-estados-hover-secondary",
-    text: "text-white",
-    buttonHighlight: Colors.estados.hover.secondary,
-  },
-  success: {
-    button: "bg-estados-normal-success active:bg-estados-hover-success",
-    text: "text-white",
-    buttonHighlight: Colors.estados.hover.success,
-  },
-  danger: {
-    button: "bg-estados-normal-danger active:bg-estados-hover-danger",
-    text: "text-white",
-    buttonHighlight: Colors.estados.hover.danger,
-  },
-  warning: {
-    button: "bg-estados-normal-warning active:bg-estados-hover-warning",
-    text: "text-black",
-    buttonHighlight: Colors.estados.hover.warning,
-  },
-  info: {
-    button: "bg-estados-normal-info active:bg-estados-hover-info",
-    text: "text-black",
-    buttonHighlight: Colors.estados.hover.info,
-  },
-  light: {
-    button: "bg-estados-normal-light active:bg-estados-hover-light",
-    text: "text-black",
-    buttonHighlight: Colors.estados.hover.light,
-  },
-  dark: {
-    button: "bg-estados-normal-dark active:bg-estados-hover-dark",
-    text: "text-white",
-    buttonHighlight: Colors.estados.hover.dark,
-  },
 };
 
 const Button = ({
   className,
-  color,
-  variant,
+  color = "light",
+  variant = "solid",
   text,
   slots,
   fullWidth = false,
+  ...props
 }: ButtonProps) => {
-  const theme = useMemo(() => {
-    if (color && colors[color]) {
-      return colors[color];
-    }
-    return colors.secondary;
-  }, [color]);
+  const theme = useMemo(() => buttonColors[color][variant], [color, variant]);
 
   return (
-    <TouchableHighlight
+    <Pressable
       className={cn(
         "min-w-2 p-2 rounded-lg items-center",
         theme.button,
@@ -100,10 +53,19 @@ const Button = ({
       style={{
         width: "auto",
       }}
-      underlayColor={theme.buttonHighlight}
+      {...props}
     >
-      <Text className={cn(theme.text, slots?.text?.className)}>{text}</Text>
-    </TouchableHighlight>
+      {({ pressed }) => (
+        <Text
+          className={cn(
+            pressed ? theme.text.pressed : theme.text.default,
+            slots?.text?.className
+          )}
+        >
+          {text}
+        </Text>
+      )}
+    </Pressable>
   );
 };
 
---
+++ src/components/button/styles.ts
@@ -0,0 +1,175 @@
+import Colors from "@constants/Colors";
+
+export const buttonColors = {
+  main: {
+    solid: {
+      button: "bg-brand-main active:bg-brand-secondary",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.brand.secondary,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-brand-main active:bg-brand-secondary",
+      text: {
+        default: "text-brand-main",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.brand.secondary,
+    },
+  },
+  primary: {
+    solid: {
+      button: "bg-estados-normal-primary active:bg-estados-hover-primary",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.primary,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-primary active:bg-estados-hover-primary",
+      text: {
+        default: "text-estados-normal-primary",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.primary,
+    },
+  },
+  secondary: {
+    solid: {
+      button: "bg-estados-normal-secondary active:bg-estados-hover-secondary",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.secondary,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-secondary active:bg-estados-hover-secondary",
+      text: {
+        default: "text-estados-normal-secondary",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.secondary,
+    },
+  },
+  success: {
+    solid: {
+      button: "bg-estados-normal-success active:bg-estados-hover-success",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.success,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-success active:bg-estados-hover-success",
+      text: {
+        default: "text-estados-normal-success",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.success,
+    },
+  },
+  danger: {
+    solid: {
+      button: "bg-estados-normal-danger active:bg-estados-hover-danger",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.danger,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-danger active:bg-estados-hover-danger",
+      text: {
+        default: "text-estados-normal-danger",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.danger,
+    },
+  },
+  warning: {
+    solid: {
+      button: "bg-estados-normal-warning active:bg-estados-hover-warning",
+      text: {
+        default: "text-black",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.warning,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-warning active:bg-estados-hover-warning",
+      text: {
+        default: "text-estados-normal-warning",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.warning,
+    },
+  },
+  info: {
+    solid: {
+      button: "bg-estados-normal-info active:bg-estados-hover-info",
+      text: {
+        default: "text-black",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.info,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-info active:bg-estados-hover-info",
+      text: {
+        default: "text-estados-normal-info",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.info,
+    },
+  },
+  light: {
+    solid: {
+      button: "bg-estados-normal-light active:bg-estados-hover-light",
+      text: {
+        default: "text-black",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.light,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-light active:bg-estados-hover-light",
+      text: {
+        default: "text-estados-normal-light",
+        pressed: "text-black",
+      },
+      buttonHighlight: Colors.estados.hover.light,
+    },
+  },
+  dark: {
+    solid: {
+      button: "bg-estados-normal-dark active:bg-estados-hover-dark",
+      text: {
+        default: "text-white",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.dark,
+    },
+    outline: {
+      button:
+        "bg-transparent border border-estados-normal-dark active:bg-estados-hover-dark",
+      text: {
+        default: "text-estados-normal-dark",
+        pressed: "text-white",
+      },
+      buttonHighlight: Colors.estados.hover.dark,
+    },
+  },
+};
