23fa419743e2ea691bde8252c60bef5d589dba11
AzaradoChan <65982758+vitorpsogarcia@users.noreply.github.com>
2025-08-22 22:20:57 -0300

    [ FIX ] Grid de propriedades

--- src/components/properties/index.tsx
+++ src/components/properties/index.tsx
@@ -30,7 +30,7 @@ const Properties = ({ data }: PropertiesProps) => {
           <ChevronDireita iconSize="sm" />
         </View>
       </View>
-      <View className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 font-sans">
+      <View className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 font-sans">
         {data?.map((item, index) => {
           return (
             <TouchableHighlight
